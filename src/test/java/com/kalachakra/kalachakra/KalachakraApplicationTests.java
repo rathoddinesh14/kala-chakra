b6d48272dbf94d8e16559e8be3150483c6e4729c
Rathod Dinesh <85003035+rathoddinesh14@users.noreply.github.com>
2024-10-25 20:50:39 +0530

    added basic rate limiter (#2)

---
+++ src/test/java/com/kalachakra/kalachakra/KalachakraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalachakra.kalachakra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalachakraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
